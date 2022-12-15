8e028fbdc4225d48f1af7066b4545f553d3bb994
Nikita <nikita_rudakov_03@mail.ru>
2022-12-15 20:48:10 +0300

    Add filter into transaction_filter.py.

--- data/transaсtion_filter.py
+++ data/transaсtion_filter.py
@@ -1,9 +1,9 @@
 from data.__all_models import *
-from datetime import datetime
+from datetime import date, timedelta
 
 
-def get_transactions(session, client_id, account_id=None, card_id=None, month=None,
-                     mcc_type=None, city=None, merchant_name=None):
+def get_transactions(session, client_id: int, account_id: int = None, card_id: int = None, month: int = None,
+                     mcc_type: str = None, city: str = None, merchant_name: str = None):
     transactions = session.query(transaction.Transaction). \
         outerjoin(card.Card, transaction.Transaction.card_id == card.Card.id). \
         outerjoin(account.Account, transaction.Transaction.card_id == account.Account.id). \
@@ -12,13 +12,10 @@ def get_transactions(session, client_id, account_id=None, card_id=None, month=No
         outerjoin(mcc.Mcc, merchant.Merchant.mcc_id == mcc.Mcc.id).filter(client.Client.id == client_id)
     transactions = transactions.filter(account.Account.id == account_id) if account_id else transactions
     transactions = transactions.filter(card.Card.id == card_id) if card_id else transactions
-    # transactions = transactions.filter()
+    transactions = transactions.filter(date(2022, month, 1) <= transaction.Transaction.datetime,
+                                       transaction.Transaction.datetime <= timedelta(days=1)) if month else transactions
     transactions = transactions.filter(mcc.Mcc.mcc_type == mcc_type) if mcc_type else transactions
     transactions = transactions.filter(merchant.Merchant.merchant_city == city) if city else transactions
     transactions = transactions.filter(
         merchant.Merchant.merchant_name == merchant_name) if merchant_name else transactions
-    # if month:
-    #     date = transaction.Transaction.datetime
-    #     transactions = transactions.filter(datetime(2022, 11, 1) <= date) \
-    #         .filter(date <= datetime(2022, 12, 1))
-    # return list(transactions)
+    return list(transactions)
